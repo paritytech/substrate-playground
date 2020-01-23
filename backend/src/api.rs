7e4b49649b3714172f528aea8e805ac1f6ca01fa
Julien Eluard <julien.eluard@gmail.com>
2020-01-23 13:23:37 +0100

    Fixed metrics

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -9,28 +9,30 @@ use rocket::{get, State};
 use rocket_contrib::{json, json::JsonValue};
 use rocket_prometheus::prometheus::{opts, IntCounterVec};
 
+// Prometheus metrics definition
+
 pub static UNKNOWN_TEMPLATE_COUNTER: Lazy<IntCounterVec> = Lazy::new(|| {
-    IntCounterVec::new(opts!("unknown_template_counter", "Count of unknown template"), &["new"])
+    IntCounterVec::new(opts!("unknown_template_counter", "Count of unknown template"), &["template"])
         .expect("Could not create lazy IntCounterVec")
 });
 
 pub static DEPLOY_COUNTER: Lazy<IntCounterVec> = Lazy::new(|| {
-    IntCounterVec::new(opts!("deploy_counter", "Count of deployments"), &["new"])
+    IntCounterVec::new(opts!("deploy_counter", "Count of deployments"), &["template", "uuid"])
         .expect("Could not create lazy IntCounterVec")
 });
 
 pub static DEPLOY_FAILURES_COUNTER: Lazy<IntCounterVec> = Lazy::new(|| {
-    IntCounterVec::new(opts!("deploy_failures_counter", "Count of deployment failures"), &["new"])
+    IntCounterVec::new(opts!("deploy_failures_counter", "Count of deployment failures"), &["template"])
         .expect("Could not create lazy IntCounterVec")
 });
 
 pub static UNDEPLOY_COUNTER: Lazy<IntCounterVec> = Lazy::new(|| {
-    IntCounterVec::new(opts!("undeploy_counter", "Count of undeployments"), &["new"])
+    IntCounterVec::new(opts!("undeploy_counter", "Count of undeployments"), &["template", "uuid"])
         .expect("Could not create lazy IntCounterVec")
 });
 
 pub static UNDEPLOY_FAILURES_COUNTER: Lazy<IntCounterVec> = Lazy::new(|| {
-    IntCounterVec::new(opts!("undeploy_failures_counter", "Count of undeployments failures"), &["new"])
+    IntCounterVec::new(opts!("undeploy_failures_counter", "Count of undeployments failures"), &["template", "uuid"])
         .expect("Could not create lazy IntCounterVec")
 });
 
@@ -58,7 +60,7 @@ pub fn index(state: State<'_, Context>, template: String) -> JsonValue {
                     .unwrap()
                     .schedule_with_delay(chrono::Duration::hours(3), move || {
                         info!("#Deleting! {}", uuid2);
-                        if let Err(s) = kubernetes::undeploy(&host, &namespace, uuid2.as_str()) {
+                        if let Err(s) = kubernetes::undeploy(&host, &namespace, &uuid2) {
                             warn!("Failed to undeploy {}: {}", uuid2, s);
                             UNDEPLOY_FAILURES_COUNTER.with_label_values(&[&template, &uuid2]).inc();
                         } else {
