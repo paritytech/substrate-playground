3f3b263faa67fb86b1614fc6ffcf8a8535128bc4
Julien Eluard <julien.eluard@gmail.com>
2020-03-23 17:56:39 +0100

    Updated dependencies

--- frontend/src/hooks.tsx
+++ frontend/src/hooks.tsx
@@ -64,7 +64,8 @@ export function useInterval(callback: () => void, delay: number) {
   }, [delay]);
 }
 
-function useLocalStorage(key: string, initialValue: string) {
+export function useLocalStorage(key: string, initialValue: string) {
+  // From https://usehooks.com/useLocalStorage/
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
@@ -78,4 +79,24 @@ function useLocalStorage(key: string, initialValue: string) {
       console.log(error);
       return initialValue;
     }
-  })};
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: String) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
