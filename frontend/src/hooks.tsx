d195392c2055e854ad040fb801c1ae4f4a9ddb53
Julien Eluard <julien.eluard@gmail.com>
2020-05-01 16:24:10 +0200

    Use a streamlined page skeleton

--- frontend/src/hooks.tsx
+++ frontend/src/hooks.tsx
@@ -1,40 +1,41 @@
-import { MutableRefObject, useState, useEffect, useRef } from "react";
+import { MutableRefObject, useCallback, useState, useEffect, useRef } from "react";
 
 /*
  * Based on https://codesandbox.io/s/usehover-v2-jxfdp
  */
-export function useHover<T>(): [MutableRefObject<T>, boolean] {
-    const [value, setValue] = useState(false);
-    const ref = useRef<any>(null);
-    const timerRef = useRef(null);
+export function useHover() {
+  const [value, setValue] = useState(false);
+	
+  // Wrap in useCallback so we can use in dependencies below
+  const handleMouseOver = useCallback(() => setValue(true), []);
+  const handleMouseOut = useCallback(() => setValue(false), []);
+
+  // Keep track of the last node passed to callbackRef
+  // so we can remove its event listeners.
+  const ref = useRef();
   
-    const handleMouseOver = () => setValue(true);
-    const handleMouseOut = () => setValue(false);
-    const config = {childList: true};
-    const observer = new MutationObserver(() => setValue(false));
-  
-    useEffect(
-      () => {
-        const node = ref.current as unknown as Node;
-        if (node) {
-          node.addEventListener('mouseover', handleMouseOver);
-          node.addEventListener('mouseout', handleMouseOut);
-          const parentNode = node.parentNode;
-          if (parentNode) {
-            observer.observe(parentNode, config);
-          }
-  
-          return () => {
-            node.removeEventListener('mouseover', handleMouseOver);
-            node.removeEventListener('mouseout', handleMouseOut);
-            observer.disconnect();
-          };
-        }
-      },
-      [ref.current] // Recall only if ref changes
-    );
-  
-    return [ref, value];
+  // Use a callback ref instead of useEffect so that event listeners
+  // get changed in the case that the returned ref gets added to
+  // a different element later. With useEffect, changes to ref.current
+  // wouldn't cause a rerender and thus the effect would run again.
+  const callbackRef = useCallback(
+    node => {
+      if (ref.current) {
+        ref.current.removeEventListener("mouseover", handleMouseOver);
+        ref.current.removeEventListener("mouseout", handleMouseOut);
+      }
+
+      ref.current = node;
+
+      if (ref.current) {
+        ref.current.addEventListener("mouseover", handleMouseOver);
+        ref.current.addEventListener("mouseout", handleMouseOut);
+      }
+    },
+    [handleMouseOver, handleMouseOut]
+  );
+
+  return [callbackRef, value];
 }
 
 export function useWindowMaxDimension() {
