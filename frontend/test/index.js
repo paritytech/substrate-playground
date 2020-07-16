e0653e9af62e56c0e718e1e86a4f914d1dc9cc8a
Julien Eluard <julien.eluard@gmail.com>
2020-07-16 09:27:15 +0200

    Ignore SSL during tests

--- frontend/test/index.js
+++ frontend/test/index.js
@@ -17,6 +17,7 @@ function playgroundDomain() {
 (async () => {
   const browser = await webkit.launch();
   const context = await browser.newContext({
+    ignoreHTTPSError: true,
     userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1'
   });
   const page = await context.newPage();
