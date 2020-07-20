48c4504d901db1e7abfbaf1bfb1fe0b8c6861cbf
Julien Eluard <julien.eluard@gmail.com>
2020-07-20 14:29:51 +0200

    [FIX #162] Makse sure query parameters are never lost

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -1,3 +1,5 @@
+import { History } from 'history';
+
 function timeout<T>(promise: Promise<T>, ms: number): Promise<T> {
     return new Promise(function(resolve, reject) {
       setTimeout(function() {
@@ -9,4 +11,15 @@ function timeout<T>(promise: Promise<T>, ms: number): Promise<T> {
 
 export async function fetchWithTimeout(url: string, opts: Object = {cache: "no-store"}, ms: number = 30000): Promise<Response>  {
     return timeout(fetch(url, opts), ms).catch(error => error);
+}
+
+export function navigateToHomepage(history: History): void {
+  history.push("/");
+}
+
+export function navigateToInstance(history: History, instanceUUID: string): void {
+  const params = new URLSearchParams(location.search);
+  params.delete("deploy");
+  const query = params.toString();
+  history.push(`/${instanceUUID}${query !== "" ? "?"+query  : ""}`);
 }
