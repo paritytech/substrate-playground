1b2a805d678f72645cc5ae389daf742d46069d76
Julien Eluard <julien.eluard@gmail.com>
2020-09-18 09:44:24 +0200

    Added CLI progress report

--- cli/ui.js
+++ cli/ui.js
@@ -6,36 +6,59 @@ const Markdown = require('ink-markdown');
 const Gradient = require('ink-gradient');
 const BigText = require('ink-big-text');
 const Link = require('ink-link');
+const Spinner = require('ink-spinner');
 
-async function dockerRun(templateId, tag, ui, debug) {
+async function dockerRun(templateId, tag, web) {
 	const { spawn } = require('child_process');
 
-	const image = `paritytech/substrate-playground-template-${templateId}${ui?"-theia":""}:${tag}`;
-	if (ui) {
-		return spawn('docker', ['run', '-p', '80:3000' , image], debug ? {stdio: 'inherit'} : {});
+	const image = `paritytech/substrate-playground-template-${templateId}${web?"-theia":""}:${tag}`;
+	if (web) {
+		return spawn('docker', ['run', '-p', '80:3000' , image]);
 	} else {
 		return spawn('docker', ['run', '-it', image, 'bash'], {stdio: 'inherit'});
 	}
 }
 
-function Cartouche({web, templateId}) {
+function WebCartouche({ state, templateId }) {
+	switch (state) {
+		case STATE_INIT:
+		case STATE_DOWNLOADING:
+			return <Text>Downloading image for <Text bold color="green">{templateId}</Text> <Spinner.default /></Text>;
+		case STATE_DOWNLOADING:
+		case STATE_STARTING:
+			return <Text>Starting <Text bold color="green">{templateId}</Text></Text>;
+		case STATE_STARTED:
+			return (
+				<>
+					<Text><Text bold color="green">{templateId}</Text> started</Text>
+					<Link url="http://localhost">Browse <Text bold>http://localhost</Text></Link>
+					<Text>Hit <Text color="red" bold>Ctrl+c</Text> to exit</Text>
+				</>
+			);
+	}
+}
+
+function CLICartouche({ templateId }) {
+	return <Text>Hit <Text color="red" bold>Ctrl+d</Text> to exit</Text>;
+}
+
+function Cartouche({state, web, templateId}) {
 	return (
 		<Box borderStyle="double" borderColor="green" flexDirection="column" margin={2}>
 			<Box flexDirection="column" alignItems="center" justifyContent="center" margin={1}>
 				{web
-				?<>
-					<Link url="http://localhost">Browse <Text bold color="green">{templateId}</Text> at <Text bold>localhost</Text></Link>
-					<Text>Hit <Text color="red" bold>Ctrl+c</Text> to exit</Text>
-				 </>
-				:<>
-					<Text>Starting <Text bold color="green">{templateId}</Text></Text>
-					<Text>Hit <Text color="red" bold>Ctrl+d</Text> to exit</Text>
-				 </>}
-
+				 ? <WebCartouche state={state} templateId={templateId} />
+				 : <CLICartouche templateId={templateId} />}
 			</Box>
 		</Box>
 	);
 }
+
+const STATE_INIT = "INIT";
+const STATE_DOWNLOADING = "DOWNLOADING";
+const STATE_DOWNLOADED = "DOWNLOADED";
+const STATE_STARTING = "STARTING";
+const STATE_STARTED = "STARTED";
 
 const App = (object) => {
 	const web = object.web;
@@ -47,6 +70,7 @@ const App = (object) => {
 	const [description, setDescription] = useState(templates[templateIds[0]].description);
 	const [templateId, setTemplateId] = useState(object.template);
 	const template = templates[templateId];
+	const [state, setState] = useState(STATE_INIT);
 
 	const handleSelect = (template) => {
 		setTemplateId(template.value);
@@ -60,6 +84,20 @@ const App = (object) => {
 		async function deploy(template) {
 			const tag = template.image.split(":").slice(-1)[0];
 			const p = await dockerRun(templateId, tag, web, debug);
+			if (p.stdout) {
+				p.stdout.on('data', function(data) {
+					const s = data.toString();
+					if (s.match(/.*Status: Downloaded newer image.*/)) {
+						setState(STATE_DOWNLOADED);
+					} else if (s.match(/.*Unable to find image.*/)) {
+						setState(STATE_DOWNLOADING);
+					} else if (s.match(/.*INFO Deploy plugins.*/)) {
+						setState(STATE_STARTED);
+					} else if (s.match(/.*INFO Configuration directory URI.*/)) {
+						setState(STATE_STARTING);
+					}
+				});
+			}
 			p.on('exit', () => console.log('\nTemplate shut down'));
 		}
 
@@ -67,9 +105,10 @@ const App = (object) => {
 			deploy(template);
 		}
 	}, [templateId]);
-	
+
 	return (
 	<Box flexDirection="column">
+
 		<Box flexDirection="column">
 			<Gradient name="rainbow">
 				<BigText text="Playground"/>
@@ -91,7 +130,7 @@ const App = (object) => {
 		</Box>}
 
 		{template &&
-		<Cartouche web={web} templateId={templateId} />}
+		<Cartouche state={state} web={web} templateId={templateId} />}
 
 		{(templateId && !template) &&
 		<Box borderStyle="double" borderColor="red" flexDirection="column" margin={2}>
