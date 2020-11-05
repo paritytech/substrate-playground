7107d714ebc6edb8ba65a9296dabc9d7707952f6
Julien Eluard <julien.eluard@gmail.com>
2020-11-05 09:57:20 +0100

    Added basic admin page

--- frontend/src/admin.tsx
+++ frontend/src/admin.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import { useLifecycle } from './lifecycle';
-import { Wrapper } from './components';
+import { InstanceDetails, Wrapper } from './components';
+import { Container } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 export function AdminPanel({ client }) {
     const location = useLocation();
     const history = useHistory();
     const [state, send] = useLifecycle(history, location, client);
     const details = state.context.details;
+    const instances = Object.entries(details ? details["all_instances"] : {});
     return (
         <div style={{ display: "flex", width: "100vw", height: "100vh", alignItems: "center", justifyContent: "center"}}>
             <Wrapper client={client} send={send} details={details}>
+                <Container style={{ display: "flex", flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Paper style={{ display: "flex", flexDirection: "column", marginTop: 20, justifyContent: "center"}} elevation={3}>
+                        <Grid container justify="center" spacing={1} style={{margin: 10}}>
+                        {instances.map((entry, index) => {
+                            return (
+                                <Grid key={index} item>
+                                    <Container style={{display: "flex", flex: 1, flexDirection: "column", padding: 0, justifyContent: "center", alignItems: "center", overflowY: "auto"}}>
+                                        <Typography variant="h5">{entry[0]}</Typography>
+                                        <InstanceDetails instance={entry[1]} />
+                                    </Container>
+                                </Grid>
+                            );
+                        })}
+                        </Grid>
+                    </Paper>
+                </Container>
             </Wrapper>
         </div>
     );
