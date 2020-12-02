8dc30e087c5cafffd97712120cdb34a51e5690e0
Julien Eluard <julien.eluard@gmail.com>
2020-12-02 17:37:45 +0100

    Introduced github module

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -1,25 +1,18 @@
 //! HTTP endpoints exposed in /api context
 
+use crate::github::token_validity;
 use crate::user::{Admin, User};
 use crate::Context;
-use hyper::{
-    header::{qitem, Accept, Authorization, Basic, UserAgent},
-    mime::Mime,
-    net::HttpsConnector,
-    status::StatusCode,
-    Client,
-};
 use rocket::request::{self, FromRequest, Request};
 use rocket::response::{content, status, Redirect};
 use rocket::{
-    catch, delete, get,
+    catch, delete, get, put,
     http::{Cookie, Cookies, SameSite, Status},
     post, Outcome, State,
 };
-use rocket_contrib::{json, json::JsonValue};
+use rocket_contrib::{json, json::{Json, JsonValue}};
 use rocket_oauth2::{OAuth2, TokenResponse};
 use serde::Serialize;
-use std::io::Read;
 use tokio::runtime::Runtime;
 
 const COOKIE_TOKEN: &str = "token";
@@ -37,43 +30,6 @@ pub struct GitHubUser {
     login: String,
     #[serde(default)]
     avatar_url: String,
-}
-
-fn token_validity(token: &str, client_id: &str, client_secret: &str) -> Result<GitHubUser, String> {
-    let https = HttpsConnector::new(hyper_sync_rustls::TlsClient::new());
-    let client = Client::with_connector(https);
-
-    let mime = "application/vnd.github.v3+json"
-        .parse()
-        .expect("parse GitHub MIME type");
-
-    // https://docs.github.com/en/rest/reference/apps#check-a-token
-    let response: hyper::client::response::Response = client
-        .post(format!("https://api.github.com/applications/{}/token", client_id).as_str())
-        .header(Authorization(Basic {
-            username: client_id.to_owned(),
-            password: Some(client_secret.to_owned()),
-        }))
-        .header(Accept(vec![qitem(mime)]))
-        .header(UserAgent("Substrate Playground".into()))
-        .body(format!("{{\"access_token\":\"{}\"}}", token).as_str())
-        .send()
-        .map_err(|op| {
-            format!(
-                "Failed to access Playground GitHub application: {}",
-                op.to_string()
-            )
-        })?;
-
-    if response.status == StatusCode::Ok {
-        let token_validity: GitHubTokenValidity =
-            serde_json::from_reader(response.take(2 * 1024 * 1024)).map_err(|error| {
-                format!("Failed to read GitHubTokenValidity: {}", error.to_string())
-            })?;
-        Ok(token_validity.user)
-    } else {
-        Err("Invalid token".to_string())
-    }
 }
 
 // Extract a User from private cookies
@@ -193,25 +149,25 @@ pub fn get_unlogged(state: State<'_, Context>) -> JsonValue {
 #[get("/users")]
 pub fn list_users(state: State<'_, Context>, admin: Admin) -> JsonValue {
     let manager = state.manager.clone();
-    result_to_jsonrpc(manager.get(user))
+    result_to_jsonrpc(manager.get_admin(admin))
 }
 
-#[put("/users", data= JSon<User>)]
-pub fn create_user(state: State<'_, Context>, admin: Admin) -> JsonValue {
+#[put("/users", data= "<user>")]
+pub fn create_user(state: State<'_, Context>, admin: Admin, user: Json<User>) -> JsonValue {
     let manager = state.manager.clone();
-    result_to_jsonrpc(manager.get(user))
+    result_to_jsonrpc(manager.get_admin(admin))
 }
 
 #[put("/users/<id>")]
-pub fn update_user(state: State<'_, Context>, admin: Admin) -> JsonValue {
+pub fn update_user(state: State<'_, Context>, admin: Admin, id: String) -> JsonValue {
     let manager = state.manager.clone();
-    result_to_jsonrpc(manager.get(user))
+    result_to_jsonrpc(manager.get_admin(admin))
 }
 
 #[delete("/users/<id>")]
-pub fn delete_user(state: State<'_, Context>, admin: Admin) -> JsonValue {
+pub fn delete_user(state: State<'_, Context>, admin: Admin, id: String) -> JsonValue {
     let manager = state.manager.clone();
-    result_to_jsonrpc(manager.get(user))
+    result_to_jsonrpc(manager.get_admin(admin))
 }
 
 /// Deploy `template` Docker container for `user_id`.
@@ -255,32 +211,6 @@ pub fn post_install_callback(
     token: TokenResponse<GitHubUser>,
     mut cookies: Cookies<'_>,
 ) -> Result<Redirect, String> {
-    let https = HttpsConnector::new(hyper_sync_rustls::TlsClient::new());
-    let client = Client::with_connector(https);
-
-    // Use the token to retrieve the user's GitHub account information.
-    let mime: Mime = "application/vnd.github.v3+json"
-        .parse()
-        .expect("parse GitHub MIME type");
-    let response: hyper::client::response::Response = client
-        .get("https://api.github.com/user")
-        .header(Authorization(format!("token {}", token.access_token())))
-        .header(Accept(vec![qitem(mime)]))
-        .header(UserAgent("Substrate Playground".into()))
-        .send()
-        .map_err(|error| {
-            format!(
-                "Failed to access GitHub user profile: {}",
-                error.to_string()
-            )
-        })?;
-
-    if !response.status.is_success() {
-        return Err(format!(
-            "Error when accessing GitHub user profile:  {}",
-            response.status
-        ));
-    }
 
     cookies.add_private(
         Cookie::build(COOKIE_TOKEN, token.access_token().to_string())
