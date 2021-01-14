8d1e3e7cd9bbc414eb1a678dc7066ca82c03e0b7
Julien Eluard <julien.eluard@gmail.com>
2021-01-14 10:17:29 +0100

    Resolved type conflict

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,12 +1,12 @@
-export enum Environment {dev, staging, production}
+export enum EnvironmentType {dev, staging, production}
 
-export function playgroundBaseURL(env: Environment) {
+export function playgroundBaseURL(env: EnvironmentType) {
     switch (env) {
-        case Environment.dev:
+        case EnvironmentType.dev:
             return "http://playground-dev.substrate.test/api";
-        case Environment.staging:
+        case EnvironmentType.staging:
             return "https://playground-staging.substrate.dev/api";
-        case Environment.production:
+        case EnvironmentType.production:
             return "https://playground.substrate.dev/api";
         default:
             throw new Error(`Unrecognized env ${env}`);
