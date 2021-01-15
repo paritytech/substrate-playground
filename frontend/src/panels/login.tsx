75630927a395f75fe69595a71e4dc9b064cf98ea
Julien Eluard <julien.eluard@gmail.com>
2021-01-15 16:00:11 +0100

    Propagate all query params

--- frontend/src/panels/login.tsx
+++ frontend/src/panels/login.tsx
@@ -5,17 +5,18 @@ import Typography from '@material-ui/core/Typography';
 import { CenteredContainer } from "../components";
 
 function login(): void {
-    // TODO propagate deploy and files params
-    window.location.href = "/api/login/github";
+    window.location.href = `/api/login/github${window.location.search}`;
 }
 
 export function LoginPanel(): JSX.Element {
     return (
         <CenteredContainer>
-            <Typography variant="h3" style= {{ textAlign: "center" }}>
-                You must log in to use Playground
-            </Typography>
-            <Button style={{ marginTop: 40 }} startIcon={<GitHubIcon />} onClick={login} color="primary" variant="contained" disableElevation>LOGIN</Button>
+            <>
+                <Typography variant="h3" style= {{ textAlign: "center" }}>
+                    You must log in to use Playground
+                </Typography>
+                <Button style={{ marginTop: 40 }} startIcon={<GitHubIcon />} onClick={login} color="primary" variant="contained" disableElevation>LOGIN</Button>
+            </>
         </CenteredContainer>
     );
 }
