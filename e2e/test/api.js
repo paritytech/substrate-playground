745aaf7ab213d7d7d1e4ef64c8538fbd5743b9d8
Julien Eluard <julien.eluard@gmail.com>
2021-01-15 11:22:11 +0100

    Fixed client usage

--- e2e/test/api.js
+++ e2e/test/api.js
@@ -1,16 +1,16 @@
 import test from 'ava';
 import pkg from '@substrate/playground-client';
-const { Client } = pkg;
+const { Client, playgroundBaseURL } = pkg;
 
 const env = process.env.ENV || "dev";
-const client = new Client({env: env});
+const client = new Client(playgroundBaseURL(env), {credentials: "include"});
 
 test('unauthenticated - should not be able to create a new session', async (t) => {
-  const res = await client.deployInstance('node-template');
+  const res = await client.createOrUpdateCurrentSession({template: 'node-template'});
   t.is(res.error, 'User unauthorized');
 });
 
 test('unauthenticated - should be able to list templates', async (t) => {
-  const res = await client.getDetails();
+  const res = await client.get();
   t.is(Object.keys(res.result.templates).length > 0, true);
 });
