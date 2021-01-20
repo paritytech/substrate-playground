397ea30f32ec417542d188bba854c07066220597
Julien Eluard <julien.eluard@gmail.com>
2021-01-20 09:28:09 +0100

    Only access user when needed

--- backend/src/manager.rs
+++ backend/src/manager.rs
@@ -223,12 +223,15 @@ impl Manager {
         username: &str,
         conf: SessionConfiguration,
     ) -> Result<Session, String> {
-        let user = self
-            .clone()
-            .get_user(username)?
-            .ok_or_else(|| format!("No user {}", username))?;
-        if conf.duration.is_some() && !self.can_update_duration(user) {
-            return Err("Only admin can customize a session duration".to_string());
+        if conf.duration.is_some() {
+            // Duration can only customized by users with proper rights
+            let user = self
+                .clone()
+                .get_user(username)?
+                .ok_or_else(|| format!("Duration customization requires user but can't find {}", username))?;
+            if ! self.can_update_duration(user) {
+                return Err("Only admin can customize a session duration".to_string());
+            }
         }
 
         let template = conf.clone().template;
