58773352b205e53c72763b37d43eb5f8ff3e9857
Julien Eluard <julien.eluard@gmail.com>
2022-02-15 16:17:08 +0100

    Added resources requirements

--- backend/src/kubernetes.rs
+++ backend/src/kubernetes.rs
@@ -182,12 +182,12 @@ fn create_pod(
                 env: Some(pod_env_variables(template, &env.host, session_id)),
                 resources: Some(ResourceRequirements {
                     requests: Some(BTreeMap::from([
-                        ("memory".to_string(), Quantity("50Gi".to_string())),
-                        ("ephemeral-storage".to_string(), Quantity("50Gi".to_string())),
+                        ("memory".to_string(), Quantity("32Gi".to_string())),
+                        ("ephemeral-storage".to_string(), Quantity("25Gi".to_string())),
                         ("cpu".to_string(), Quantity("0.5".to_string())),
                     ])),
                     limits: Some(BTreeMap::from([
-                        ("memory".to_string(), Quantity("100Gi".to_string())),
+                        ("memory".to_string(), Quantity("64Gi".to_string())),
                         ("ephemeral-storage".to_string(), Quantity("50Gi".to_string())),
                         ("cpu".to_string(), Quantity("1".to_string())),
                     ])),
