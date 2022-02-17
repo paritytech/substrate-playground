108544c09d271993ebe03be87aac21032d6787a3
Julien Eluard <julien.eluard@gmail.com>
2022-02-17 15:13:03 +0100

    Format

--- backend/src/kubernetes.rs
+++ backend/src/kubernetes.rs
@@ -367,12 +367,18 @@ fn create_pod(
                 resources: Some(ResourceRequirements {
                     requests: Some(BTreeMap::from([
                         ("memory".to_string(), Quantity("32Gi".to_string())),
-                        ("ephemeral-storage".to_string(), Quantity("25Gi".to_string())),
+                        (
+                            "ephemeral-storage".to_string(),
+                            Quantity("25Gi".to_string()),
+                        ),
                         ("cpu".to_string(), Quantity("0.5".to_string())),
                     ])),
                     limits: Some(BTreeMap::from([
                         ("memory".to_string(), Quantity("64Gi".to_string())),
-                        ("ephemeral-storage".to_string(), Quantity("50Gi".to_string())),
+                        (
+                            "ephemeral-storage".to_string(),
+                            Quantity("50Gi".to_string()),
+                        ),
                         ("cpu".to_string(), Quantity("1".to_string())),
                     ])),
                     ..Default::default()
