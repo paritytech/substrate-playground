0aa279171a1d8849f438f6ca7b261a8cb2cb484b
Julien Eluard <julien.eluard@gmail.com>
2022-02-22 11:37:16 +0100

    Updated client

--- e2e/test/api.ts
+++ e2e/test/api.ts
@@ -1,9 +1,14 @@
 import test from 'ava';
-import { Client, EnvironmentType, playgroundBaseURL } from '@substrate/playground-client';
+import { Client, EnvironmentType, playgroundBaseURL, environmentTypeFromString } from '@substrate/playground-client';
+
 import 'cross-fetch/dist/node-polyfill.js'; // TODO remove once moving to Node18 (https://github.com/nodejs/node/pull/41749)
 
-const env = EnvironmentType[process.env.ENV];
-const client = new Client(playgroundBaseURL(env), 30000, {credentials: "include"});
+const env = environmentTypeFromString(process.env.ENV);
+const accessToken = process.env.ACCESS_TOKEN;
+
+function newClient(): Client {
+    return new Client(playgroundBaseURL(env), 30000, {credentials: "include"});
+}
 
 // Disable certificate checking for 'dev' env
 if (env == EnvironmentType.dev) {
@@ -12,14 +17,23 @@ if (env == EnvironmentType.dev) {
 
 test('unauthenticated - should not be able to create a new workspace', async (t) => {
     try {
-        await client.createCurrentWorkspace({repositoryDetails: {id: 'node-template', reference: ''}});
+        await newClient().createCurrentWorkspace({repositoryDetails: {id: 'node-template', reference: ''}});
         t.fail('Can create a workspace w/o login');
     } catch {
         t.pass();
     }
 });
 
+test('authenticated - should be able to get current session', async (t) => {
+    const client = newClient();
+    await client.login(accessToken);
+
+    await client.getCurrentSession();
+
+    t.pass();
+});
+
 test('unauthenticated - should be able to list templates', async (t) => {
-    const details = await client.get();
+    const details = await newClient().get();
     t.is(Object.keys(details.templates).length > 0, true);
 });
