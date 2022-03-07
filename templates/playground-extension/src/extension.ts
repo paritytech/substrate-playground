c42ce17d3608494397844f34e80e606ee5af1ee7
Julien Eluard <julien.eluard@gmail.com>
2022-03-07 17:04:03 +0100

    Some vscode experiment

--- templates/playground-extension/src/extension.ts
+++ templates/playground-extension/src/extension.ts
@@ -2,6 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
+import { Client } from '@substrate/playground-client';
+import * as Octokit from '@octokit/rest';
+
+import 'cross-fetch/dist/node-polyfill.js';
 
 function parseCommands(command: any): Array<string> {
     if (typeof command === "string") {
@@ -14,9 +18,85 @@ function parseCommands(command: any): Array<string> {
     return [];
 }
 
+export async function conectToPlayground(id: string) {
+    const uri = vscode.Uri.parse(`vscode-remote://ssh-remote+${id}.playground.substrate.dev/`);
+    await vscode.commands.executeCommand('vscode.openFolder', uri, true);
+}
+
+class GitpodWorksapcePorts extends vscode.TreeItem {
+	readonly ports = new Map<number, number>();
+	constructor() {
+		super('Ports', vscode.TreeItemCollapsibleState.Expanded);
+	}
+}
+
+type GitpodWorkspaceElement = GitpodWorksapcePorts;
+
+export class GitpodWorkspaceTreeDataProvider implements vscode.TreeDataProvider<GitpodWorkspaceElement> {
+
+	readonly ports = new GitpodWorksapcePorts();
+
+	protected readonly onDidChangeTreeDataEmitter = new vscode.EventEmitter<GitpodWorkspaceElement | undefined>();
+	readonly onDidChangeTreeData = this.onDidChangeTreeDataEmitter.event;
+
+	constructor(
+		private readonly context: vscode.ExtensionContext
+	) {
+	}
+
+	getTreeItem(element: GitpodWorkspaceElement): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: GitpodWorkspaceElement): vscode.ProviderResult<GitpodWorkspaceElement[]> {
+		if (!element) {
+			return [this.ports];
+		}
+		return [];
+	}
+
+	getParent(element: GitpodWorkspaceElement): GitpodWorkspaceElement | undefined {
+		return undefined;
+	}
+
+}
+
+function apiHost(): string {
+    const paths = window.location.host.split('.');
+    paths.shift();
+    return `https://${paths.join('.')}/api`;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	const gitpodWorkspaceTreeDataProvider = new GitpodWorkspaceTreeDataProvider(context);
+	const workspaceView = vscode.window.createTreeView('github.playground.explorer', {
+		treeDataProvider: gitpodWorkspaceTreeDataProvider,
+	});
+    context.subscriptions.push(workspaceView);
+
+    const details = await new Client("https://playground.substrate.dev/api"/*apiHost()*/, 30000, {credentials: "include"}).get();
+    console.log(details);
+
+
+    const session = await vscode.authentication.getSession('github', ['user:email'], { createIfNone: true });
+    console.log(session);
+
+    context.subscriptions.push(vscode.authentication.onDidChangeSessions(async e => {
+        console.log(e)
+    }));
+
+    if (session) {
+        const octokit = new Octokit.Octokit({
+            auth: session.accessToken
+        });
+        const userInfo = await octokit.users.getAuthenticated();
+
+		vscode.window.showInformationMessage(`Logged into GitHub as ${userInfo.data.login}`);
+    }
+
+
  if (vscode.workspace.workspaceFolders) {
     const data = await vscode.workspace.fs.readFile(vscode.Uri.file(path.resolve(vscode.workspace.workspaceFolders[0].uri.path, '.devcontainer/devcontainer.json')));
     // TODO filter comments from JSON
