9a4c489ce53cdcca6c6003e0a1cb97878882d4f8
Julien Eluard <julien.eluard@gmail.com>
2022-04-12 12:54:45 +0200

    Reintroduced templates

---
+++ frontend/src/panels/admin/templates.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { Client, LoggedUser, Template } from '@substrate/playground-client';
+import { useStyles, EnhancedTableToolbar, Resources } from '.';
+
+export function Templates({ client, user }: { client: Client, user?: LoggedUser }): JSX.Element {
+    const classes = useStyles();
+
+    return (
+        <Resources<Template> callback={async () => (await client.listTemplates())}>
+        {(resources: Template[]) => (
+            <>
+                <EnhancedTableToolbar user={user} label="Templates" />
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Image</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {Object.entries(resources).map(([id, template]) => (
+                        <TableRow key={id}>
+                            <TableCell component="th" scope="row">
+                                {template.name}
+                            </TableCell>
+                            <TableCell>{template.name}</TableCell>
+                            <TableCell>{template.image}</TableCell>
+                        </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </>
+            )}
+        </Resources>
+    );
+}
