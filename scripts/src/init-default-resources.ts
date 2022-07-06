f9e9c4d65d3a0f60c38a688c838bfdef21451c4e
Julien Eluard <julien.eluard@gmail.com>
2022-07-06 11:20:36 +0200

    Disable certificate checking for 'dev' env

--- scripts/src/init-default-resources.ts
+++ scripts/src/init-default-resources.ts
@@ -1,17 +1,23 @@
 
-import { Client, playgroundBaseURL, environmentTypeFromString, mainSessionId } from '@substrate/playground-client';
+import { Client, playgroundBaseURL, environmentTypeFromString, mainSessionId, EnvironmentType } from '@substrate/playground-client';
 import 'cross-fetch/dist/node-polyfill.js'; // TODO remove once moving to Node18 (https://github.com/nodejs/node/pull/41749)
 
 const accessToken = process.env.ACCESS_TOKEN;
 
-function newClient(): Client {
-    const env = environmentTypeFromString(process.env.ENV);
+const env = environmentTypeFromString(process.env.ENV);
+
+function newClient(env: EnvironmentType): Client {
     return new Client(playgroundBaseURL(env), 30000, {credentials: "include"});
+}
+
+// Disable certificate checking for 'dev' env
+if (env == EnvironmentType.dev) {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 }
 
 // Connect via Client, create others Role, feed repository
 
-const client = newClient();
+const client = newClient(env);
 try {
     await client.login(accessToken);
 
