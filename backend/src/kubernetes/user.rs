d4a4159bef64e7bc98d00a76d5532803dfd9990c
Julien Eluard <julien.eluard@gmail.com>
2022-09-16 16:23:10 +0200

    Fmt

--- backend/src/kubernetes/user.rs
+++ backend/src/kubernetes/user.rs
@@ -150,7 +150,7 @@ pub async fn create_user(id: &str, conf: UserConfiguration) -> Result<()> {
                             name: "backend-ui-service".to_string(),
                             port: Some(ServiceBackendPort {
                                 name: Some("ui-port".to_string()),
-                                number: Some(80)
+                                number: Some(80),
                             }),
                         }),
                         ..Default::default()
