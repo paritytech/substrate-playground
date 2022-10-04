e11fb68da38d8b931fa6a7bb8a2b195a37284889
Julien Eluard <julien.eluard@gmail.com>
2022-10-04 21:15:36 +0200

    Attempt to configure settings.json

--- resources/editors/openvscode/start.sh
+++ resources/editors/openvscode/start.sh
@@ -3,4 +3,4 @@
 scriptdir="$(dirname "$0")"
 cd "$scriptdir"
 
-bin/openvscode-server --port ${PORT} --host 0.0.0.0 --without-connection-token --disable-telemetry
+bin/openvscode-server --port ${PORT} --host 0.0.0.0 --without-connection-token --disable-telemetry --user-data-dir data
